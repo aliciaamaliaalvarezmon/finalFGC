d6a5d4f63c08e774e79f87b0a5f937f39222833e
aliciaamaliaalvarezmon <aliciaysuerte@gmail.com>
2021-10-11 02:48:22 -0300

    version 1 finalizada

--- trabajoFinalPrueba/version6/visualization.js
+++ trabajoFinalPrueba/version6/visualization.js
@@ -540,26 +540,105 @@ void main () {
 
    function vertexShader() {
     return `
-        attribute float vertexDisplacement;
-        varying vec3 vUv; 
+        attribute float vertexDisplacement;        
         varying vec2 vao; 
         uniform float musicAvg;
         uniform float musicHigh;
         uniform float musicLow;
+        varying float vWave;
 
-        uniform float time;  
+        uniform float time; 
+
+        //  Simplex 3D Noise 
+//  by Ian McEwan, Ashima Arts
+//
+vec4 permute(vec4 x){return mod(((x*34.0)+1.0)*x, 289.0);}
+vec4 taylorInvSqrt(vec4 r){return 1.79284291400159 - 0.85373472095314 * r;}
+
+float snoise(vec3 v){ 
+  const vec2  C = vec2(1.0/6.0, 1.0/3.0) ;
+  const vec4  D = vec4(0.0, 0.5, 1.0, 2.0);
+
+// First corner
+  vec3 i  = floor(v + dot(v, C.yyy) );
+  vec3 x0 =   v - i + dot(i, C.xxx) ;
+
+// Other corners
+  vec3 g = step(x0.yzx, x0.xyz);
+  vec3 l = 1.0 - g;
+  vec3 i1 = min( g.xyz, l.zxy );
+  vec3 i2 = max( g.xyz, l.zxy );
+
+  //  x0 = x0 - 0. + 0.0 * C 
+  vec3 x1 = x0 - i1 + 1.0 * C.xxx;
+  vec3 x2 = x0 - i2 + 2.0 * C.xxx;
+  vec3 x3 = x0 - 1. + 3.0 * C.xxx;
+
+// Permutations
+  i = mod(i, 289.0 ); 
+  vec4 p = permute( permute( permute( 
+             i.z + vec4(0.0, i1.z, i2.z, 1.0 ))
+           + i.y + vec4(0.0, i1.y, i2.y, 1.0 )) 
+           + i.x + vec4(0.0, i1.x, i2.x, 1.0 ));
+
+// Gradients
+// ( N*N points uniformly over a square, mapped onto an octahedron.)
+  float n_ = 1.0/7.0; // N=7
+  vec3  ns = n_ * D.wyz - D.xzx;
+
+  vec4 j = p - 49.0 * floor(p * ns.z *ns.z);  //  mod(p,N*N)
+
+  vec4 x_ = floor(j * ns.z);
+  vec4 y_ = floor(j - 7.0 * x_ );    // mod(j,N)
+
+  vec4 x = x_ *ns.x + ns.yyyy;
+  vec4 y = y_ *ns.x + ns.yyyy;
+  vec4 h = 1.0 - abs(x) - abs(y);
+
+  vec4 b0 = vec4( x.xy, y.xy );
+  vec4 b1 = vec4( x.zw, y.zw );
+
+  vec4 s0 = floor(b0)*2.0 + 1.0;
+  vec4 s1 = floor(b1)*2.0 + 1.0;
+  vec4 sh = -step(h, vec4(0.0));
+
+  vec4 a0 = b0.xzyw + s0.xzyw*sh.xxyy ;
+  vec4 a1 = b1.xzyw + s1.xzyw*sh.zzww ;
+
+  vec3 p0 = vec3(a0.xy,h.x);
+  vec3 p1 = vec3(a0.zw,h.y);
+  vec3 p2 = vec3(a1.xy,h.z);
+  vec3 p3 = vec3(a1.zw,h.w);
+
+//Normalise gradients
+  vec4 norm = taylorInvSqrt(vec4(dot(p0,p0), dot(p1,p1), dot(p2, p2), dot(p3,p3)));
+  p0 *= norm.x;
+  p1 *= norm.y;
+  p2 *= norm.z;
+  p3 *= norm.w;
+
+// Mix final noise value
+  vec4 m = max(0.6 - vec4(dot(x0,x0), dot(x1,x1), dot(x2,x2), dot(x3,x3)), 0.0);
+  m = m * m;
+  return 42.0 * dot( m*m, vec4( dot(p0,x0), dot(p1,x1), 
+                                dot(p2,x2), dot(p3,x3) ) );
+} 
 
         
 
         void main() {
-
-        vUv = position; 
-        vec3 transformed = vec3(position);
-
+        
         vao= uv;
 
+        vec3 pos = position;       
+        float noiseFreq = 3.5;
+        float noiseAmp = musicAvg; 
+        vec3 noisePos = vec3(pos.x * noiseFreq + time, pos.y, pos.z);
+        pos.z += snoise(noisePos) * noiseAmp;
+        vWave = pos.z; // Off it goes!
 
 
+        vec3 transformed = vec3(position);
         if(musicAvg>0.0){             
             transformed.x = position.x - musicAvg*5.0;            
 
@@ -578,15 +657,21 @@ void main () {
       varying vec2 vao; 
       uniform float musicAvg;
       uniform sampler2D spectrum; 
-      varying vec3 vUv;
+      varying float vWave;      
+
+    
 
 
-      void main() {
-
+    void main() { 
+        float wave = vWave * 0.0007;
+        float r = texture2D(spectrum, vao).r;
+        float g = texture2D(spectrum, vao+ wave).g;
+        float b = texture2D(spectrum, vao).b;
+        // Put them back together
+        vec3 texture = vec3(r, g, b);
+        //vec3 texture = texture2D(spectrum, vao + wave).rgb;           
         
-        
-        float colorMix = smoothstep(1.0, 2.0, vUv.z);
-        gl_FragColor = texture2D(spectrum, vao);
+        gl_FragColor = vec4(texture, musicAvg/100.0);
         
       }
     `
@@ -604,15 +689,26 @@ var frequencyClone = frequencyArray;
 
 
 
-
+var j = 0.0;
 for ( let i = 0; i < size; i ++ ) {
 
     const stride = i * 3;
     
 
     data[ stride ] = frequencyClone[i%256];
-    data[ stride + 1 ] = 0.0;
+    /*if(musicAvg>0){
+        data[ stride + 1 ] = j;
+    }else{
+        data[ stride + 1 ] = 0.0;
+    }*/
+    data[ stride + 1 ] = j;
     data[ stride + 2 ] = 0.0;
+
+    if(j <100.0){
+        j++;
+    }else{
+        j=0.0;
+    }
 
 
 
@@ -647,6 +743,8 @@ function dummy_update(scene, lowerAvgFr){
         vec4 modelViewPosition = modelViewMatrix * vec4(position+vec3(musicAvg, musicAvg,musicAvg), 1.0);
         gl_Position = projectionMatrix * modelViewPosition ; 
         }
+
+         gl_FragColor = texture2D(spectrum, vao);
 
         
     `*/
